384eb5a3a1f2f2a41044c7fee40546c99d9b91a0
g1usmin <leon_rossonero@hotmail.com>
2023-05-15 20:24:40 -0300

    Agregando clase usuario, igu y persistencia

--- src/main/java/com/todocodeacademy/login_simple/Login_simple.java
+++ src/main/java/com/todocodeacademy/login_simple/Login_simple.java
@@ -1,15 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package com.todocodeacademy.login_simple;
+
+import com.todocodeacademy.login_simple.igu.Principal;
 
 
 public class Login_simple {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+        
+        
+        
+        
     }
+
 
 }
