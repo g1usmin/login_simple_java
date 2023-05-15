f647c5d42bdcbb84f46574128abad5f8059c5518
g1usmin <leon_rossonero@hotmail.com>
2023-05-15 17:23:27 -0300

    Iniciando proyectito

---
+++ src/main/java/com/todocodeacademy/login_simple/Login_simple.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.todocodeacademy.login_simple;
+
+/**
+ *
+ * @author Gabi
+ */
+public class Login_simple {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
